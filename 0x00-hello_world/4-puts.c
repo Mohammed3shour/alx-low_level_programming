89d19ff9c768dd7dc8886517c52c0ad5d78ce4e9
Mohammed3shour <me@example.com>
2023-08-11 07:00:01 -0700

    hokey

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stduio.h>
+/**
+*main - ENTRY point 
+*description: a c program that print function
+*return :always 0 (success)
+*/
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-
-
-
-
-return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
