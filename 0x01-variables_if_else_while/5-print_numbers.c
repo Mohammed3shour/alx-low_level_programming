88a7f4208eedb0f58253ca315dddcbb262f3be02
Mohammed3shour <me@example.com>
2023-08-16 18:40:47 -0700

    passy

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:print numbers from 0 to 10
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (; num < 10; i++)
+	printf("%d\n", num);
+return (0);
+}
