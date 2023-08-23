621639bc5bcf3026c4e66b8c1531c069a83bc987
Mohammed3shour <me@example.com>
2023-08-22 18:54:54 -0700

    loney

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,6 +14,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int_atoi(chat' *s);
+int _atoi(char *s);
 
 #endif /*MAIN-H */
