521a6bcc5b9fe1275852b7dc2eda6e7d40aa08cf
Mohammed3shour <me@example.com>
2023-08-22 18:40:27 -0700

    loney

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * reset_to_98 - takes a pointer to an int parameter
+ *		and update the value it points to 98
+ *
+ * @n: int parameter
+ *
+ * Return: nothing
+*/
+
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
