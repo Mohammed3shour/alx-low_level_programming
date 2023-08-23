a720747c60f40e2d5a8e2b0e1a57681300f70b72
Mohammed3shour <me@example.com>
2023-08-22 18:56:41 -0700

    loney

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN.H
+#define MAIN.H
 
 #include <stdio.h>
 #include <unistd.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -16,4 +17,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /*MAIN-H */
+#endif
