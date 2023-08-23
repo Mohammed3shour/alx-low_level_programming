2a6920857a06a55a2453dfbe16ea712055094bc9
Mohammed3shour <me@example.com>
2023-08-22 19:40:51 -0700

    loney

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+/**
+ * print_rev - prints a reversed string, followed by a new line
+ * @s: pointer to thestring to print
+ * Return:void
+*/
+void print_rev(char *s)
+{
+	while (s[i])
+	i++;
+
+	while (i--)
+	{
+	_putchar(s[i]);
+	}
+	_putchar('\n');
+}
