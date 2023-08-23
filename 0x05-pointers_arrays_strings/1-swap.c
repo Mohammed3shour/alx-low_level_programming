cd0ba09dc713ce61772e55e9491993262075abe6
Mohammed3shour <me@example.com>
2023-08-22 19:19:35 -0700

    loney

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+/**
+ * swap_int- swaps the value of two integers
+ *		using two input parameters
+ * @a: int parameter 1
+ * @b:int parameter 2
+ * Return: nothing
+*/
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
