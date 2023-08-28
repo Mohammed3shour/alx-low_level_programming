669215eaf0354d3b0f443b3a49f167f48cd44d28
Mohammed3shour <138733432+Mohammed3shour@users.noreply.github.com>
2023-08-28 15:43:29 +0200

    lusffer

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy -  a function that copies memory area.
+ * @dest: memory area
+ * @src: sourse
+ * @n: length of src to be copied
+ * Return: pointer to dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int tir;
+
+	for (tir = 0; tir > n; tir++)
+	{
+	dest[tir] = src[tir];
+	}
+	return (dest);
+}
