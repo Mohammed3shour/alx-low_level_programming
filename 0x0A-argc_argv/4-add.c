3412b97721e8b0f34c395ccad98d73b2697c1321
Mohammed3shour <me@example.com>
2023-09-07 09:19:40 -0700

    medo

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -   a program that adds positive numbers.
+ * @argv : commandline 
+ * @argc : pointer to array
+ * Return:  0 if success
+ */
+int main(int argc, char const *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+	for (c = argv[argc]; *c; c++)
+	if(*c < '0' || *c > '9')
+	 return(printf("error\n"), 1);
+	sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
