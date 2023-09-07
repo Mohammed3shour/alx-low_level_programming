adc5ef895bacc8904ff9e2511f611ce60866c78c
Mohammed3shour <me@example.com>
2023-09-07 03:42:45 +0200

    medo

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,10 @@
 nclude <stdio.h>
 /*
- *  * main -  a program that prints the number of arguments passed into it.
- *   * @argc : nuber of command
- *    * @argv : an array 
- *     * return : 0
- *     */
+ * main -  a program that prints the number of arguments passed into it.
+ * @argc : nubmer of command
+ * @argv : an array 
+ * return 0
+*/
 int main(int argc, char const *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
