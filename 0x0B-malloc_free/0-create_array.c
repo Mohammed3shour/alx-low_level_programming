45552a27f7cf2c0c87c3d6e752ab8ca6eaeabc46
medo <mohamed@example.com>
2024-01-28 20:37:04 -0800

    meso

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * creat_array-creats an array of charand initialized it
+ * @size: size of array
+ * @c: char to initialized 
+ * Return: initialized or null array
+*/
+
+char*creat_array(unsigned int size, char c)	
+{
+	char *n = malloc(size)
+	if (size == 0 || n == 0)
+	return (0);
+
+	while (size--)
+	n[size] = c;
+	return (n);
+
+}
