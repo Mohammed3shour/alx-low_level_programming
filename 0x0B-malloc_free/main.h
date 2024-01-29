69f5c92e84be19534e3d7e43848e3495a18dad51
medo <mohamed@example.com>
2024-01-28 20:46:17 -0800

    meso

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -12,4 +13,4 @@ void free_grid(int **grid, int hight);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
 
-#endif /*main.h_medo*/
+#endif /*main.h*/
