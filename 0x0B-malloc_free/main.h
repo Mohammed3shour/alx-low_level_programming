f26a21655da11bbc04ef81cc8b0760efe5b87b21
medo <mohamed@example.com>
2024-01-28 20:43:56 -0800

    meso

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
-char*creat_array(unsigned int size, char c);
+char *creat_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int hight);
